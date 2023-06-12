e602f0ac17f0d00874f404aff3bb50cdb92db42d
ade-m <66313509+ade-m@users.noreply.github.com>
2023-06-12 20:02:23 +0700

    Komposisi

--- src/App.java
+++ src/App.java
@@ -4,9 +4,7 @@ import model.Mobil;
 public class App {
     public static void main(String[] args) throws Exception {
         Mobil mobil = new Mobil();
-        Engine engine = new Engine();
-        Engine engine2 = new Engine();
-        mobil.setEngine(engine2);
+
         mobil.start();
         mobil.start();
         mobil.stop();
